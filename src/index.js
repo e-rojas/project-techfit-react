96010dd6795a2e6f0588c0982593f794f64f81a0
anthonyshadow <anthony.shadowitz@gmail.com>
2019-11-22 15:17:53 -0500

    added Whychooseus and Register into the components on homepage

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Test from './pages/TestPageAnthony'
 import Home from './pages/home'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,17 @@ import { Container } from 'react-bootstrap'
 import TopNavBar from '../components/navbar'
 import "bootstrap/dist/css/bootstrap.min.css";
 import About from '../components/aboutUs'
+import Register from '../components/Register'
+import WhyChooseUs from '../components/WhyChooseUs'
+
 const home = () => {
     return (
         <Container className="bg-warning " fluid={true}>
             <TopNavBar />
             <About />
+            <WhyChooseUs />
+            <Register />
+
         </Container>
     );
 }
