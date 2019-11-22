694d26f33032feedfaebc2885c4b66244f0dc6a7
javascript01fan <ejrojas@icloud.com>
2019-11-22 12:57:43 -0500

    home and profile page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-<<<<<<< HEAD
-import Test from './pages/edgarTestPageComp'
-=======
-import Test from './pages/TestPageGriffin'
->>>>>>> 289788c9fbb5d1d839e77d99c33fe3990796988d
+//import Test from './pages/edgarTestPageComp'
+import Home from './pages/home'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
