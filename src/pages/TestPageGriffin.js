2fcc4b19393e409c51198e814bab825749c1eedf
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-22 14:02:09 -0500

    updated

--- src/pages/TestPageGriffin.js
+++ src/pages/TestPageGriffin.js
@@ -1,7 +1,18 @@
 import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Navbar,
+  NavDropdown,
+  Nav,
+  Form,
+  FormControl
+} from "react-bootstrap";
 import Tracker from '../components/liquids/Tracker';
+
 const initialState = { count: 0 };
 
 function reducer(state, action) {
@@ -14,16 +25,21 @@ function reducer(state, action) {
       throw new Error();
   }
 }
-
 //----- css style variable.
 
 const colStyle = { marginTop: "100px" };
 const rowStyle = { minHeight: "60vh" };
-const footerButton = {display:'flex',flexDirection:'row',width:'100%',justifyContent:'space-evenly',padding:'40px'}
+const footerButton = {
+  display: "flex",
+  flexDirection: "row",
+  width: "100%",
+  justifyContent: "space-evenly",
+  padding: "40px"
+};
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <Container className="bg-warning p-5" fluid={true}>
+    <Container className="bg-warning " fluid={true}>
       <Row className="bg-dark p-4">
         <Col style={colStyle} lg={8} className="bg-info">
           Column One
@@ -35,8 +51,38 @@ export default function App() {
           Column Two
         </Col>
       </Row>
+
+      {/* Navigation bar */}
+
+      <Row >
+        <Col className='p-0'>
+          <Navbar fixed="top" className='pl-5 pr-5 pt-4 pb-4 border-bottom shadow-lg ' bg="light" expand="lg">
+            <Navbar.Brand className='font-weight-bolder' href="#home">Techfit</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              
+              <Nav className="ml-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Button variant="dark">Login</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Col>
+      </Row>
+
+      {/*  Navigation bar*/}
+
       <Row style={rowStyle} className="bg-light p-4">
-        <Col style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}} lg={6} className="">
+        <Col
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+          lg={6}
+          className=""
+        >
           <header>
             <h1>Techfit</h1>
             <p>To inspire healthy living in a sedentary world.</p>
@@ -47,18 +93,16 @@ export default function App() {
             <span>Instagram Icon</span>
             <span>Youtube Icon</span>
           </div>
-
           <footer style={footerButton}>
-            <Button  variant="outline-dark">Leearn More</Button>
-            <Button  variant="outline-dark">Register</Button>
+            <Button variant="outline-dark">Leearn More</Button>
+            <Button variant="outline-dark">Register</Button>
           </footer>
         </Col>
         <Col lg={6} className="bg-danger">
-          <section className='liquids'>
-            <Tracker 
-            />
+          <section className="liquids">
+            <Tracker />
           </section>
-         <img src="" alt=""/>
+          <img src="" alt="" />
         </Col>
       </Row>
     </Container>
