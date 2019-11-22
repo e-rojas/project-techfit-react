cf347c9d22c3f768d11d19ce55c19add3b923292
anthonyshadow <anthony.shadowitz@gmail.com>
2019-11-22 13:06:57 -0500

    add the why choose us component to the homepage

--- src/pages/TestPageAnthony.js
+++ src/pages/TestPageAnthony.js
@@ -17,7 +17,9 @@ function reducer(state, action) {
 
 const colStyle = { marginTop: "100px" };
 const rowStyle = { minHeight: "60vh" };
+const whyChooseStyle = {minHeight: "30vh"}
 const footerButton = {display:'flex',flexDirection:'row',width:'100%',justifyContent:'space-evenly',padding:'40px'}
+const imageStyle = { height: "180px", width: "180px"}
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   return (
@@ -54,6 +56,29 @@ export default function App() {
          <img src="" alt=""/>
         </Col>
       </Row>
+      <Row className="bg-light p-4">
+        <Col style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+          <h1>Why Choose Us?</h1>
+        </Col>
+      </Row>
+      <Row style={{whyChooseStyle } }className="bg-light p-4 text-center">
+        <Col lg={4}>
+          <img  style = {imageStyle} src="images/idea.svg" alt="Think Smart"></img>
+          <h4>Meal & Fitness Profile</h4>
+          <p>We have extensive experience and can be proud of 10 000+ completed projects.</p>
+        </Col>
+        <Col lg={4}>
+          <img  style = {imageStyle} src="images/analysis.svg" alt="Progress Report"></img>
+          <h4>Progress Reports</h4>
+          <p>We value each client and always respond to feedback throughout our cooperation.</p>
+        </Col>
+        <Col lg={4}>
+          <img style = {imageStyle}  src="images/startup.svg" alt="Rocket"></img>
+          <h4>Tech Space Friendly</h4>
+          <p>We create our products using the latest technologies to ensure the best experience.</p>
+        </Col>
+      </Row>
+
     </Container>
   );
 }
