c1025cf64a6102f60dceac002b9b71a34e94e167
javascript01fan <ejrojas@icloud.com>
2019-11-23 13:30:56 -0500

    fitness page and componenets created

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-import Test from "./pages/MealPlan";
+import Test from "./pages/FitnessPlan";
 
 import * as serviceWorker from "./serviceWorker";
 
