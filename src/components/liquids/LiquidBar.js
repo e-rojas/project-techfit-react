2acae88664cd8bcb41e935e55aa496f5fe707c0a
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-25 15:00:09 -0500

    code to create each bar for stacked graph separated to helper file

--- src/components/liquids/LiquidBar.js
+++ src/components/liquids/LiquidBar.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { VictoryChart, VictoryAxis, VictoryLine, VictoryBar, VictoryStack, VictoryTheme } from 'victory';
+import GenerateBar from './GenerateBar.js';
 
 export default function LiquidBar(props) {
   return (
@@ -8,100 +9,36 @@ export default function LiquidBar(props) {
       <VictoryChart 
         height={500}
         width={500}
-        theme={VictoryTheme.material}
-      >        
-      {/* X AXIS */}
-      <VictoryAxis crossAxis
-        domain={[0, 7]}
-        theme={VictoryTheme.material}
-        offsetY={50}
-        standalone={false}
-      />
-
-      {/* Y AXIS */}
-      <VictoryAxis dependentAxis crossAxis
-        domain={[0, 15]}
-        theme={VictoryTheme.material}
-        offsetX={50}
-        standalone={false}
-      />
-    
-      {/* LINE GRAPH WITH DEFAULT DATA */}
-      <VictoryLine />
-
-      {/* STACKED BAR CHART */}
-      <VictoryStack>
-        {/* WATER */}
-        <VictoryBar
-          alignment={'end'}
-          animate={{
-            duration: 1500,
-            onLoad: { duration: 1500 }
-          }}
-          style={{
-            data: { fill: 'cyan', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
-            labels: { fontSize: 6 },
-          }}
-          data={[
-            {x: 1, y: 4}, 
-            {x: 2, y: 1}, 
-            {x: 3, y: 3}, 
-            {x: 4, y: 5},
-            {x: 5, y: 2},
-            {x: 6, y: 2},
-            {x: 7, y: 4}
-          ]}
+        theme={VictoryTheme.material}>
+                  
+        {/* X AXIS */}
+        <VictoryAxis crossAxis
+          domain={[0, 7]}
+          theme={VictoryTheme.material}
+          offsetY={50}
+          standalone={false}
         />
-        {/* COFFEE */}
-        <VictoryBar
-          alignment={'end'}
-          animate={{
-            duration: 1500,
-            onLoad: { duration: 1500 }
-          }}
-          style={{
-            data: { fill: 'tan', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
-            labels: { fontSize: 6 },
-          }}
-          data={[
-            {x: 1, y: 1}, 
-            {x: 2, y: 3}, 
-            {x: 3, y: 1}, 
-            {x: 4, y: 1},
-            {x: 5, y: 2},
-            {x: 6, y: 1},
-            {x: 7, y: 3}
-          ]}
-        />
-        {/* SODA */}
-        <VictoryBar
-          alignment={'end'}
-          animate={{
-            duration: 1500,
-            onLoad: { duration: 1500 }
-          }}
-          style={{
-            data: { fill: 'tomato', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
-            labels: { fontSize: 6 },
-          }}
-          data={[
-            {x: 1, y: 0}, 
-            {x: 2, y: 2}, 
-            {x: 3, y: 0}, 
-            {x: 4, y: 1},
-            {x: 5, y: 1},
-            {x: 6, y: 0},
-            {x: 7, y: 3}
-          ]}
-        />
-      </VictoryStack>
 
-    {/* data={[
-        [{x: 1, y: 4}, {x: 2, y: 3}, {x: 3, y: 2}, {x: 4, y: 6}, {x: 5, y: 3}, {x: 6, y: 4}, {x: 7, y: 3}],
-        [{x: 1, y: 1}, {x: 2, y: 5}, {x: 3, y: 3}, {x: 4, y: 1}, {x: 5, y: 0}, {x: 6, y: 1}, {x: 7, y: 1}],
-        [{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 1}, {x: 4, y: 1}, {x: 5, y: 3}, {x: 6, y: 0}, {x: 7, y: 2}],
-        [{x: 1, y: 0}, {x: 2, y: 1}, {x: 3, y: 2}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 7, y: 1}]
-      ]} */}
+        {/* Y AXIS */}
+        <VictoryAxis dependentAxis crossAxis
+          domain={[0, 15]}
+          theme={VictoryTheme.material}
+          offsetX={50}
+          standalone={false}
+        />
+      
+        {/* LINE GRAPH WITH DEFAULT DATA */}
+        <VictoryLine />
+
+        {/* STACKED BAR CHART */}
+        <VictoryStack>
+          {/* WATER */
+          GenerateBar('water')}
+          {/* COFFEE */
+          GenerateBar('coffee')}
+          {/* SODA */
+          GenerateBar('soda')}
+        </VictoryStack>
       </VictoryChart>
     </Fragment>
   )
