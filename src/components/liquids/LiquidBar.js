67c5621b664840786c60844e3293a4eb87cd28c6
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-25 14:28:55 -0500

    stacked dataset shows in bar graph

--- src/components/liquids/LiquidBar.js
+++ src/components/liquids/LiquidBar.js
@@ -9,9 +9,6 @@ export default function LiquidBar(props) {
         height={500}
         width={500}
         theme={VictoryTheme.material}
-        style={{
-          x: 'days'
-        }}
       >        
       {/* X AXIS */}
       <VictoryAxis crossAxis
@@ -23,45 +20,88 @@ export default function LiquidBar(props) {
 
       {/* Y AXIS */}
       <VictoryAxis dependentAxis crossAxis
-        domain={[0, 10]}
+        domain={[0, 15]}
         theme={VictoryTheme.material}
         offsetX={50}
         standalone={false}
       />
-      
-      
+    
       {/* LINE GRAPH WITH DEFAULT DATA */}
       <VictoryLine />
 
       {/* STACKED BAR CHART */}
       <VictoryStack>
-        {/* BAR SET 1 */}
-        <VictoryBar 
-          alignment={'start'}
+        {/* WATER */}
+        <VictoryBar
+          alignment={'end'}
           animate={{
             duration: 1500,
             onLoad: { duration: 1500 }
           }}
           style={{
-            data: { fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
+            data: { fill: 'cyan', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
             labels: { fontSize: 6 },
           }}
+          data={[
+            {x: 1, y: 4}, 
+            {x: 2, y: 1}, 
+            {x: 3, y: 3}, 
+            {x: 4, y: 5},
+            {x: 5, y: 2},
+            {x: 6, y: 2},
+            {x: 7, y: 4}
+          ]}
         />
-
-        {/* BAR SET 2 */}
-        <VictoryBar 
-          alignment={'start'}
+        {/* COFFEE */}
+        <VictoryBar
+          alignment={'end'}
           animate={{
             duration: 1500,
             onLoad: { duration: 1500 }
           }}
           style={{
-            data: { fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
-            labels: { fontSize: 6 }
+            data: { fill: 'tan', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
+            labels: { fontSize: 6 },
           }}
+          data={[
+            {x: 1, y: 1}, 
+            {x: 2, y: 3}, 
+            {x: 3, y: 1}, 
+            {x: 4, y: 1},
+            {x: 5, y: 2},
+            {x: 6, y: 1},
+            {x: 7, y: 3}
+          ]}
         />
+        {/* SODA */}
+        <VictoryBar
+          alignment={'end'}
+          animate={{
+            duration: 1500,
+            onLoad: { duration: 1500 }
+          }}
+          style={{
+            data: { fill: 'tomato', fillOpacity: 0.7, stroke: '#636363', strokeWidth: 0.1 },
+            labels: { fontSize: 6 },
+          }}
+          data={[
+            {x: 1, y: 0}, 
+            {x: 2, y: 2}, 
+            {x: 3, y: 0}, 
+            {x: 4, y: 1},
+            {x: 5, y: 1},
+            {x: 6, y: 0},
+            {x: 7, y: 3}
+          ]}
+        />
+      </VictoryStack>
 
-        </VictoryStack>
+    {/* data={[
+        [{x: 1, y: 4}, {x: 2, y: 3}, {x: 3, y: 2}, {x: 4, y: 6}, {x: 5, y: 3}, {x: 6, y: 4}, {x: 7, y: 3}],
+        [{x: 1, y: 1}, {x: 2, y: 5}, {x: 3, y: 3}, {x: 4, y: 1}, {x: 5, y: 0}, {x: 6, y: 1}, {x: 7, y: 1}],
+        [{x: 1, y: 1}, {x: 2, y: 2}, {x: 3, y: 1}, {x: 4, y: 1}, {x: 5, y: 3}, {x: 6, y: 0}, {x: 7, y: 2}],
+        [{x: 1, y: 0}, {x: 2, y: 1}, {x: 3, y: 2}, {x: 4, y: 0}, {x: 5, y: 0}, {x: 6, y: 0}, {x: 7, y: 1}]
+      ]} */}
       </VictoryChart>
     </Fragment>
   )
