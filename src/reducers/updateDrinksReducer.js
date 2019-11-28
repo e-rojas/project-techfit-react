b047b4086cd5a8e3588c780dfc66f28a9925d113
Griffin Alcorn <griffin.alcorn@gmail.com>
2019-11-28 10:28:15 -0500

    imports updated

--- src/reducers/updateDrinksReducer.js
+++ src/reducers/updateDrinksReducer.js
@@ -1,7 +1,18 @@
+import { DEFAULT_ECDH_CURVE } from "tls";
+
 export default function reducer(state, action) {
   //REDUCER
   //responds to dispatch call
+  //each case returns the new state
   switch(action.type) {
+    case 'INCREASE':
+      console.log('increase dispatched')
+      return action.value
 
+    case 'DECREASE':
+      return action.value
+
+    default:
+      throw new Error('invalid dispatch action')
   }
 }
